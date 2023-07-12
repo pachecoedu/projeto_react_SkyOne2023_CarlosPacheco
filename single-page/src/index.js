a92c30815bde58a8770318a0527a4ab6dcf7194f
pachecoedu <caedupache11@gmail.com>
2023-07-12 18:20:00 -0300

    Adicionando e salvando o valor X com o evento onClick

--- single-page/src/index.js
+++ single-page/src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 import './index.css'
 
-
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="square" onClick={() =>alert('Olha só, você clicou utilizando uma Arrow Function!')}> 
-                {this.props.value}
+            <button
+                className="square"
+                onClick={() => this.setState({ value: 'X' })}>
+                {this.state.value}
             </button>
         );
     }
 }
+
 class Board extends React.Component {
     renderSquare(i) {
         return <Square value={i} />;
