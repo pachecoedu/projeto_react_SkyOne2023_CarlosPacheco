8bba1f1001a103c097cc5fa649dd392379110716
pachecoedu <caedupache11@gmail.com>
2023-07-12 22:59:42 -0300

    Logica validação de turnos X/O com winner lose draw

--- single-page/src/index.js
+++ single-page/src/index.js
@@ -1,33 +1,75 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css'
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.setState({ value: 'X' })}>
-                {this.state.value}
-            </button>
-        );
-    }
+// renderiza um quadrado no tabuleiro
+function Square(props) {
+    //Retorna um botão com o valor e a função de clique fornecidos como props
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+            isDraw: false, 
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+        // Verificar empate apenas quando não houver vencedor.
+    if (squares.every(square => square !== null) && !calculateWinner(squares)) {
+        this.setState({ isDraw: true });
+    } else {
+        this.setState({ isDraw: false });
+    }
+    }
+
+    // render() {
+    //     return (
+    //         <button
+    //             className="square"
+    //             onClick={() => this.setState({ value: 'X' })} ========== trecho que descontinuado
+    //         >
+    //             {this.state.value}
+    //         </button>
+    //     );
+    // }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else if (this.state.isDraw) {
+            status = 'It\'s a draw!'; //adicionando mensagem de empate.
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (
             <div>
                 <div className="status">{status}</div>
@@ -63,6 +105,26 @@ class Game extends React.Component {
 
         )
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 //==================================
